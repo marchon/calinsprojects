d4df7f6df3fcb9101101b1a320e5573d1a3819fa
calin014 <calin014@d371719c-ac75-11de-9602-958045435684>
2010-05-12 07:53:56 +0000

    all

--- VR/src/ro/calin/vr/Main.java
+++ VR/src/ro/calin/vr/Main.java
@@ -10,7 +10,7 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		BaseGame game = SpaceGame.getGame();
-		//game.setConfigShowMode(ConfigShowMode.AlwaysShow);
+		game.setConfigShowMode(ConfigShowMode.AlwaysShow);
 		game.start();
 	}
 

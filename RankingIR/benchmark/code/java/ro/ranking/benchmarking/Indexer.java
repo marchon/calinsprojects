45230e310b44c21e22aa48a78732f5ec4f023700
calin014 <calin014@d371719c-ac75-11de-9602-958045435684>
2010-05-15 12:19:15 +0000

    package naming refactoring

---
+++ RankingIR/benchmark/code/java/ro/ranking/benchmarking/Indexer.java
@@ -0,0 +1,76 @@
+package ro.ranking.benchmarking;
+
+import java.util.Properties;
+
+import org.apache.lucene.benchmark.byTask.PerfRunData;
+import org.apache.lucene.benchmark.byTask.tasks.AddDocTask;
+import org.apache.lucene.benchmark.byTask.tasks.CloseIndexTask;
+import org.apache.lucene.benchmark.byTask.tasks.CreateIndexTask;
+import org.apache.lucene.benchmark.byTask.tasks.PerfTask;
+import org.apache.lucene.benchmark.byTask.tasks.RepSumByNameTask;
+import org.apache.lucene.benchmark.byTask.tasks.ResetSystemEraseTask;
+import org.apache.lucene.benchmark.byTask.tasks.TaskSequence;
+import org.apache.lucene.benchmark.byTask.utils.Config;
+
+public class Indexer {
+	public static void main(String[] args) throws Exception {
+		if(args.length != 1) {
+			System.err.println("Usage: IndexCorpus <workDir>");
+			System.exit(1);
+		}
+		
+	    Properties p = initProps(args[0]);
+	    Config conf = new Config(p);
+	    PerfRunData runData = new PerfRunData(conf);
+	    
+	    // 1. top sequence
+	    TaskSequence top = new TaskSequence(runData,null,null,false); // top level, not parallel
+	    
+	    // 2. reset all
+	    ResetSystemEraseTask reset = new ResetSystemEraseTask(runData);
+	    top.addTask(reset);
+	    
+	    // 3. task to create the index
+	    CreateIndexTask create = new CreateIndexTask(runData);
+	    top.addTask(create);
+	    
+	    // 4. task to add all docs from corpus
+	    TaskSequence seq1 = new TaskSequence(runData,"Seq",top,false);
+	    seq1.setRepetitions(TaskSequence.REPEAT_EXHAUST);
+	    seq1.setNoChildReport();
+	    PerfTask addDoc = new AddDocTask(runData);
+	    seq1.addTask(addDoc);
+	    top.addTask(seq1);
+	    
+	    // 5. task to close the index
+	    CloseIndexTask close = new CloseIndexTask(runData);
+	    top.addTask(close);
+
+	    // task to report
+	    RepSumByNameTask rep = new RepSumByNameTask(runData);
+	    top.addTask(rep);
+
+	    // print algorithm
+	    System.out.println(top.toString());
+	    
+	    // execute
+		top.doLogic();
+	  }
+
+	  private static Properties initProps(String workingDir) {
+	    Properties p = new Properties();
+	    p.setProperty ( "work.dir"  				, workingDir );
+	    p.setProperty ( "content.source"			, "org.apache.lucene.benchmark.byTask.feeds.TrecContentSource" );
+	    p.setProperty ( "content.source.log.step" 	, "2500" );
+	    p.setProperty ( "analyzer"            		, "org.apache.lucene.analysis.standard.StandardAnalyzer" );
+	    p.setProperty ( "directory"           		, "FSDirectory" );
+	    p.setProperty ( "doc.tokenized"       		, "true" );
+	    p.setProperty ( "doc.stored"          		, "true" );
+	    //p.setProperty ( "doc.body.stored"			, "false");
+	    p.setProperty ( "doc.term.vector"        	, "false" );
+	    p.setProperty ( "content.source.forever"	, "false" );
+	    p.setProperty ( "content.source.encoding"	, "UTF-8" );
+	    p.setProperty ( "content.source.excludeIteration", "true" );
+	    return p;
+	  }
+}
---
+++ RankingIR/benchmark/code/java/ro/ranking/technique/axiomatic/QualityQueryParserImpl.java
@@ -0,0 +1,73 @@
+package ro.ranking.technique.axiomatic;
+
+import java.util.List;
+
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.benchmark.quality.QualityQuery;
+import org.apache.lucene.benchmark.quality.QualityQueryParser;
+import org.apache.lucene.index.Term;
+import org.apache.lucene.queryParser.ParseException;
+import org.apache.lucene.queryParser.QueryParser;
+import org.apache.lucene.search.BooleanClause;
+import org.apache.lucene.search.BooleanQuery;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.util.Version;
+
+public class QualityQueryParserImpl implements QualityQueryParser {
+	private String qqNames[];
+	private String indexField;
+	ThreadLocal<AXQueryParser> queryParser = new ThreadLocal<AXQueryParser>();
+
+	private float avdl;
+
+	public QualityQueryParserImpl(String[] qqNames, String indexField, float avdl) {
+		this.qqNames = qqNames;
+		this.indexField = indexField;
+		this.avdl = avdl;
+	}
+	
+	static class AXQueryParser extends QueryParser {
+		private float avdl;
+		public AXQueryParser(Version matchVersion, String f, Analyzer a, float avdl) {
+			super(matchVersion, f, a);
+			this.avdl = avdl;
+		}
+		
+		@Override
+		protected Query newTermQuery(Term term) {
+			return new AXTermQuery(term, avdl, 0.35f, 0.5f);
+		}
+		
+		@Override
+		protected Query getBooleanQuery(List<BooleanClause> clauses)
+				throws ParseException {
+			//disable coord; TODO: see what this means
+			return getBooleanQuery(clauses, true);
+		}
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.apache.lucene.benchmark.quality.QualityQueryParser#parse(org.apache
+	 * .lucene.benchmark.quality.QualityQuery)
+	 */
+	public Query parse(QualityQuery qq) throws ParseException {
+		AXQueryParser qp = queryParser.get();
+		if (qp == null) {
+			qp = new AXQueryParser(Version.LUCENE_CURRENT, indexField,
+					new StandardAnalyzer(Version.LUCENE_CURRENT), avdl);
+			queryParser.set(qp);
+		}
+		
+		
+		BooleanQuery bq = new BooleanQuery();
+		for (int i = 0; i < qqNames.length; i++)
+			bq.add(qp.parse(QueryParser.escape(qq.getValue(qqNames[i]))),
+					BooleanClause.Occur.SHOULD);
+
+		return bq;
+	}
+}

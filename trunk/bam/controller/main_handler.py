c7950eb0a6ce3949c95e6f3644c4a73cad8d0ab4
calin014@gmail.com <calin014@gmail.com@d371719c-ac75-11de-9602-958045435684>
2011-12-16 12:36:37 +0000

    some jquery

--- trunk/bam/controller/main_handler.py
+++ trunk/bam/controller/main_handler.py
@@ -1,8 +1,15 @@
+from google.appengine.api import users
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import template
 import os
+import logging
 
 class MainHandler(webapp.RequestHandler):
 	def get(self):
-		path = os.path.join(os.path.dirname(__file__), '../view/index.html')
-		self.response.out.write(template.render(path, {}))
+		user = users.get_current_user()
+		if user:
+			logging.info('User ' + user.nickname() + ' requested app.')
+			path = os.path.join(os.path.dirname(__file__), '../view/index.html')
+			self.response.out.write(template.render(path, {"logout" : users.create_logout_url("/")}))
+		else:
+			self.redirect(users.create_login_url(self.request.uri))

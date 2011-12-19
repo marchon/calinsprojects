7cc3c99dea35fdb0640f0bf9cfcc93467f613656
calin014@gmail.com <calin014@gmail.com@d371719c-ac75-11de-9602-958045435684>
2011-12-19 12:41:58 +0000

    js templates

--- bam/static/js/scripts.js
+++ bam/static/js/scripts.js
@@ -1,3 +1,18 @@
+//http://blogs.jetbrains.com/idea/2010/11/javascript-libraries-in-intellij-idea-10/
+
+Jaml.register('cat', function(cat){
+    div(
+        h1(cat.name),
+        p(cat.rule)
+    )
+});
+
 $(document).ready(function() {
     $("#main").accordion();
+
+    $("#test").click(function(){
+        $.get('/category?op=list', function(data) {
+            $("#testcontent").html(Jaml.render('cat', data.content));
+        }, 'json');
+    });
 });

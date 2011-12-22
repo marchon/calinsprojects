a75cb509d2adaa0e682e61f87dbffa17d786784a
calin014@gmail.com <calin014@gmail.com@d371719c-ac75-11de-9602-958045435684>
2011-12-22 21:24:12 +0000

    stuff

--- trunk/howmuch/model/model.py
+++ trunk/howmuch/model/model.py
@@ -1,3 +1,4 @@
+import datetime
 from google.appengine.api import users
 from google.appengine.ext import db
 
@@ -7,26 +8,34 @@ class Category(db.Model):
     name = db.StringProperty()
     rule = db.StringProperty()
 
-    def to_dict(self):
+    def to_dict(cat):
         return {
-            'key': str(self.key()),
-            'name': self.name,
-            'rule': self.rule
+            'key': str(cat.key()),
+            'name': cat.name,
+            'rule': cat.rule
         }
 
     def from_dict(cat):
         if cat.has_key('key'): # get from datastore
             category = db.get(cat['key'])
-            if cat['name']: category.name = cat['name']
-            if cat['rule']: category.rule = cat['rule']
+            if cat.has_key('name'): category.name = cat['name']
+            if cat.has_key('rule'): category.rule = cat['rule']
         else:         # create
             category = Category()
             category.account = users.get_current_user()
-            category.name = cat['name']
-            category.rule = cat['rule']
+            if cat.has_key('name'): category.name = cat['name']
+            if cat.has_key('rule'): category.rule = cat['rule']
 
         return category
+
+    def all_query():
+        q = Category.all()
+        q.filter('account =', users.get_current_user())
+        return q
+
+    to_dict = staticmethod(to_dict)
     from_dict = staticmethod(from_dict)
+    all_query = staticmethod(all_query)
 
 class Transaction(db.Model):
     account = db.UserProperty()
@@ -40,20 +49,40 @@ class Transaction(db.Model):
     category = db.ReferenceProperty(Category)
 
 
-    def dict(self):
+    def to_dict(trans):
         return {
-            'id': self.key().id_or_name(),
-            'date': self.date,
-            'desc': self.desc,
-            'debit': self.debit,
-            'credit': self.credit,
-            'details': dict(zip(self.detailsKeys, self.detailsValues))
-            #'category': self.category.id_or_name() #send the id??
+            'key': str(trans.key()),
+            'date': trans.date,
+            'desc': trans.desc,
+            'debit': trans.debit,
+            'credit': trans.credit,
+            'details': dict(zip(trans.detailsKeys, trans.detailsValues)),
+            #'category': trans.category #send the id??
         }
 
+    def from_dict(trans):
+        if trans.has_key('key'): # get from datastore
+            transaction = db.get(trans['key'])
+            if trans.has_key('date'): transaction.name = trans['date']
+            if trans.has_key('desc'): transaction.desc = trans['desc']
+            if trans.has_key('debit'): transaction.debit = trans['debit']
+            if trans.has_key('credit'): transaction.credit = trans['credit']
 
+        else:         # create
+            transaction = Transaction()
+            transaction.account = users.get_current_user()
+            if trans.has_key('date'): transaction.name = trans['date']
+            if trans.has_key('desc'): transaction.desc = trans['desc']
+            if trans.has_key('debit'): transaction.debit = trans['debit']
+            if trans.has_key('credit'): transaction.credit = trans['credit']
 
-entities = {
-    "Category": Category,
-    "Transaction": Transaction
-}
+        return transaction
+
+    def all_query():
+        q = Transaction.all()
+        q.filter('account =', users.get_current_user())
+        return q
+
+    to_dict = staticmethod(to_dict)
+    from_dict = staticmethod(from_dict)
+    all_query = staticmethod(all_query)

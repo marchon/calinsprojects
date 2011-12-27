8d20b19f3ea552d77ec4c2214469fbbd596a5392
calin014@gmail.com <calin014@gmail.com@d371719c-ac75-11de-9602-958045435684>
2011-12-27 17:29:26 +0000

    js refactoring added entity counter

--- howmuch/model/model.py
+++ howmuch/model/model.py
@@ -3,42 +3,44 @@ from google.appengine.api import users
 from google.appengine.ext import db
 
 class Category(db.Model):
-    account = db.UserProperty()
+    account = db.UserProperty(auto_current_user_add=True)
 
     name = db.StringProperty()
     rule = db.StringProperty()
 
-    def to_dict(cat):
+    def serialize(category):
         return {
-            'key': str(cat.key()),
-            'name': cat.name,
-            'rule': cat.rule
+            'key': str(category.key()),
+            'name': category.name,
+            'rule': category.rule
         }
 
-    def from_dict(cat):
-        if cat.has_key('key'): # get from datastore
-            category = db.get(cat['key'])
-            if cat.has_key('name'): category.name = cat['name']
-            if cat.has_key('rule'): category.rule = cat['rule']
-        else:         # create
-            category = Category()
-            category.account = users.get_current_user()
-            if cat.has_key('name'): category.name = cat['name']
-            if cat.has_key('rule'): category.rule = cat['rule']
+    def create(map):
+        category = Category()
+        if map.has_key('name'): category.name = map['name']
+        if map.has_key('rule'): category.rule = map['rule']
 
         return category
 
-    def all_query():
+    def load(map):
+        category = db.get(map['key'])
+        if map.has_key('name'): category.name = map['name']
+        if map.has_key('rule'): category.rule = map['rule']
+
+        return category
+
+    def query():
         q = Category.all()
         q.filter('account =', users.get_current_user())
         return q
 
-    to_dict = staticmethod(to_dict)
-    from_dict = staticmethod(from_dict)
-    all_query = staticmethod(all_query)
+    serialize = staticmethod(serialize)
+    create = staticmethod(create)
+    load = staticmethod(load)
+    query = staticmethod(query)
 
 class Transaction(db.Model):
-    account = db.UserProperty()
+    account = db.UserProperty(auto_current_user_add=True)
 
     date = db.DateTimeProperty()
     desc = db.StringProperty()
@@ -49,7 +51,7 @@ class Transaction(db.Model):
     category = db.ReferenceProperty(Category)
 
 
-    def to_dict(trans):
+    def serialize(trans):
         return {
             'key': str(trans.key()),
             'date': trans.date,
@@ -60,29 +62,30 @@ class Transaction(db.Model):
             #'category': trans.category #send the id??
         }
 
-    def from_dict(trans):
-        if trans.has_key('key'): # get from datastore
-            transaction = db.get(trans['key'])
-            if trans.has_key('date'): transaction.name = trans['date']
-            if trans.has_key('desc'): transaction.desc = trans['desc']
-            if trans.has_key('debit'): transaction.debit = trans['debit']
-            if trans.has_key('credit'): transaction.credit = trans['credit']
-
-        else:         # create
-            transaction = Transaction()
-            transaction.account = users.get_current_user()
-            if trans.has_key('date'): transaction.name = trans['date']
-            if trans.has_key('desc'): transaction.desc = trans['desc']
-            if trans.has_key('debit'): transaction.debit = trans['debit']
-            if trans.has_key('credit'): transaction.credit = trans['credit']
+    def create(map):
+        transaction = Transaction()
+        if map.has_key('date'): transaction.name = map['date']
+        if map.has_key('desc'): transaction.desc = map['desc']
+        if map.has_key('debit'): transaction.debit = map['debit']
+        if map.has_key('credit'): transaction.credit = map['credit']
 
         return transaction
 
-    def all_query():
+    def load(map):
+        transaction = db.get(map['key'])
+        if map.has_key('date'): transaction.name = map['date']
+        if map.has_key('desc'): transaction.desc = map['desc']
+        if map.has_key('debit'): transaction.debit = map['debit']
+        if map.has_key('credit'): transaction.credit = map['credit']
+
+        return transaction
+
+    def query():
         q = Transaction.all()
         q.filter('account =', users.get_current_user())
         return q
 
-    to_dict = staticmethod(to_dict)
-    from_dict = staticmethod(from_dict)
-    all_query = staticmethod(all_query)
+    serialize = staticmethod(serialize)
+    create = staticmethod(create)
+    load = staticmethod(load)
+    query = staticmethod(query)

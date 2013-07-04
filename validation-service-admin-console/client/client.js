0a285bbe397ee7b58968c923bdf258b284f61c3c
calin014@gmail.com <calin014@gmail.com@d371719c-ac75-11de-9602-958045435684>
2013-07-04 12:31:28 +0000

    test

--- validation-service-admin-console/client/client.js
+++ validation-service-admin-console/client/client.js
@@ -2,4 +2,10 @@ $(function(){
 	/*var editor = ace.edit("editor");
     	editor.setTheme("ace/theme/monokai");
     	editor.getSession().setMode("ace/mode/javascript");*/
+
+var source   = $("#entry-template").html();
+var template = Handlebars.compile(source);
+var context = {title: "My New Post", body: "This is my first post!"}
+var html    = template(context);
+$("body").append(html);
 });
